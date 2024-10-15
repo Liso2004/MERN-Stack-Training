4075408e751f39fd57f4b0d5b910ce733af3cdf1
Liso W.S hlatshwayo <lisowycliff@gmail.com>
2024-10-15 21:31:54 +0200

    fixed api test error and added error handling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./connfig/db.js";
+import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
 
 dotenv.config();
@@ -9,28 +9,32 @@ const app = express();
 
 app.use(express.json()); //allows us to accept JSON data in the req.body 
 
-app.post("/api/Products", async (req, res ) => {
-    const product = req.body;   //user will send this data 
-    
-    
-    if (!product.name || !product.Price || !product.image) {
-        return res.status(400).json({ success: false, Message : " Please provide  all fields "});
-    }
-    
-    const newProduct = new Product(product);
-    
+
+
+app.post("/api/products", async (req, res) => {
+
     try {
+        const product = req.body;
+
+        const newProduct = new Product(product);
+
         await newProduct.save();
-        res.status(201).json({  success : true, data : newProduct});        
+
+        res.status(201).json({ success: true, data: newProduct });
+
     } catch (error) {
-        console.error("Error in Create product:", error.Message );
-        res.status(500).json({ success : false, message: "Server Error" });  
+        if (error.name === "ValidationError") {
+            res.status(400).json({ success: false, message: "Invalid request data", errors: error.errors });
+        } else {
+            console.error("Error in Create product:", error.message);
+            res.status(500).json({ success: false, message: "Server Error" });
+        }
     }
 });
 
 
     
-app.listen(5000 , () => {
+app.listen(5000, () => {
     connectDB();
     console.log("Server started at http//localhost:5000");
 });
