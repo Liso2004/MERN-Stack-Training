c724db2091a424d450f173d1c9e4349fb8b6e943
Liso W.S hlatshwayo <lisowycliff@gmail.com>
2024-10-17 21:14:06 +0200

    tweaked server.js

--- backend/server.js
+++ backend/server.js
@@ -12,27 +12,24 @@ app.use(express.json()); //allows us to accept JSON data in the req.body
 
 
 app.post("/api/products", async (req, res) => {
+    const product = req.body;// user will send this data 
+
+    if (!product.name || !product.price || !product.image){
+        return res.status(400).json({ success:false , message: "Please provide all feilds "});
+    }
+
+const newProduct = new Product(product);
 
     try {
-        const product = req.body;
-
-        const newProduct = new Product(product);
-
         await newProduct.save();
-
-        res.status(201).json({ success: true, data: newProduct });
-
+        res.status(201).json({success : true , data: newProduct });
     } catch (error) {
-        if (error.name === "ValidationError") {
-            res.status(400).json({ success: false, message: "Invalid request data", errors: error.errors });
-        } else {
-            console.error("Error in Create product:", error.message);
-            res.status(500).json({ success: false, message: "Server Error" });
-        }
+        console.error("Error in Create product:", error.message);
+        res.status(500).json({ success: false , message: "Server Error"})
     }
 });
 
 app.listen(5000, () => {
     connectDB();
-    console.log("Server started at http//localhost:5000");
+    console.log("Server started at http://localhost:5000");
 });
