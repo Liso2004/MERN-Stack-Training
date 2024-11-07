1cadbb66ffd5f47daa7f185a8f4675f2bf07e771
Liso W.S hlatshwayo <lisowycliff@gmail.com>
2024-11-07 19:38:07 +0200

    added page routes and components and worked on the front end ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
+      <BrowserRouter> 
+       <ChakraProvider>
       <App />
-    </ChakraProvider>
+      </ChakraProvider>
+      </BrowserRouter>
   </React.StrictMode>,
 );
