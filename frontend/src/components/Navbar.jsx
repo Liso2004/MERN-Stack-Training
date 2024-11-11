61f7ba291db5fb727e56ceeed43e3fb89a38a5f6
Liso W.S hlatshwayo <lisowycliff@gmail.com>
2024-11-11 21:17:11 +0200

    added react icons and fixed the header on home page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,41 +1,45 @@
-import { Button, Container, Flex, HStack,Text } from '@chakra-ui/react';
+import { Button, Container, Flex, HStack,Text, useColorMode } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 
 import { PlusSquareIcon } from "@chakra-ui/icons";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
 
 
 
 const Navbar = () => {
-  return (<Container maxw={"1140px"} px={4}>
+  const {colorMode, toggleColorMode } = useColorMode();
+
+  return (
+    <Container maxW="1140px" px={4}>
     <Flex
-    h={16}
-    alignItems={"center"}
-    justifyContent={'space-between'}
-    flexDir={{
-      base:"column",
-      sm:"row"
-     }} 
+      height={16}
+      alignItems="center"
+      justifyContent="space-between"
+      flexDirection={{ base: "column", sm: "row" }}
     >
 
-      <Text
-      fontSize={{base: "22", sm: "28"}}
-      fontWeight={"bold"}
-      textTransform={"uppercase"}
-      textAlign={"center"}
-      bgGradient={"liner(to-r, cyan.400 , blue.500)"}
-      bgclips={"text"}
-      >
-        
-        <Link to = {"/"}>Product Store 🛒</Link>
-        </Text>
+<Text
+fontSize={{ base: "22", sm: "28" }}
+fontWeight={"bold"}
+textTransform={"uppercase"}
+textAlign={"center"}
+bgGradient={"linear(to-r, cyan.400, blue.500)"}
+bgClip={"text"}
+>
+  <Link to ={"/"}>Product Store 🛒</Link>
+     </Text>
 
-        <HStack spacing = {2} alignItems={"center"}>
+        <HStack spacing={2} alignItems={"center"}>
           <Link to = {"/create"}>
           <Button>
             <PlusSquareIcon fontSize ={20}/>
           </Button>
           </Link>
-
+          <Button onClick={toggleColorMode}>
+          {colorMode === "light" ? <IoMoon/> :
+          <LuSun Size ="20"/> 
+          }</Button>
         </HStack>
       </Flex>
   </Container>
@@ -43,3 +47,5 @@ const Navbar = () => {
 };
 
 export default Navbar
+
+// video time stamp 1:10:26 
