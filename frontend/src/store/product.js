293d471a20669cdac49be2fe9863623959e531fa
Liso W.S hlatshwayo <lisowycliff@gmail.com>
2024-12-08 20:30:35 +0200

    fixed error and added notfication on success for product added to database

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,22 +1,22 @@
 import { create } from "zustand";
 
-export const useProductStore = create ((set)=> ({
-    product:[],
-    setProduct:(product)=>set({product}),
-    createProduct: async(newProduct) => {
-        if (!newProduct.name || !newProduct.image || !newProduct.price){
-            return {success:false, Message:"Please fill in all fields."}
-        }
-        const res = await fetch("/api/products",{
-            method:"POST",
-            headers:{
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify(newProduct)
-        })
-        const data = await res.json(); 
-        set((state) => ({ products: [...state.products,data.data]}));
-        return {success:true, Message:"Product created successfully"};
-
-    } 
-}));
+export const useProductStore = create((set) => ({
+	products: [],
+	setProducts: (products) => set({ products }),
+	createProduct: async (newProduct) => {
+		if (!newProduct.name || !newProduct.image || !newProduct.price) {
+			return { success: false, message: "Please fill in all fields." };
+		}
+		const res = await fetch("/api/products", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newProduct),
+		});
+		const data = await res.json();
+		set((state) => ({ products: [...state.products, data.data] }));
+		return { success: true, message: "Product created successfully" };
+	},
+})); 
+///1:33:40 / 2:16:25 timestamp
